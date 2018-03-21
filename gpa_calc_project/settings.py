cc5cd1487c7e10993c4bb23cf38240f3a38c6728
jack-ditto <jackditto7@gmail.com>
2018-03-20 19:23:11 -0500

    Add allowed host/ uncomment link to static files

--- gpa_calc_project/settings.py
+++ gpa_calc_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w@omj+&822rox9cb0vf)!r3bz37q2s2cq02$#0ef_cs1*w_$2s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://jackditto.pythonanywhere.com/', '127.0.0.1']
 
 
 # Application definition
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
